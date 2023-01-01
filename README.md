a2756cfb98dcb2c154eea1823a44691f23ec2c1b
Stefano Tudose <stfntudose@gmail.com>
2023-01-01 09:02:57 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Minecraft Restart script
+
+With this script you can restart your server if it crashes or use a plugin that automatically closes the server (e.g. [UltimateAutoRestart](https://www.spigotmc.org/resources/1-8-1-19-3-ultimateautorestart-need-an-autorestart-plugin-grab-the-best-one-today.64414/))
+
+
+## Getting started
+
+**Check if you have Screen installed on your machine**
+1. Go to the server folder
+2. Paste the script into it
+3. Change the values ​​according to your needs
+4. Run this command `chmod +x *.sh`
+5. Run `./start.sh` 
---
+++ script.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+JAR=server.jar
+JDK=java
+MAXRAM=5G
+MINRAM=5G
+TIME=5
+
+while [ true ]; do
+    $JDK -Xmx$MAXRAM -Xms$MINRAM -jar $JAR nogui
+    if [[ ! -d "exit_codes" ]]; then
+        mkdir "exit_codes";
+    fi
+    if [[ ! -f "exit_codes/server_exit_codes.log" ]]; then
+        touch "exit_codes/server_exit_codes.log";
+    fi
+    echo "[$(date +"%d.%m.%Y %T")] ExitCode: $?" >> exit_codes/server_exit_codes.log
+    echo "-----Press enter within 5 seconds to cancel the restart-----";
+    read -t $TIME input;
+    if [ $? == 0 ]; then
+        break;
+    else
+        echo "-------------------------REBOOTING--------------------------";
+    fi
+done
